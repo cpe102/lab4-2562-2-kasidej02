a9441beca35892eb44fb02887d329a4fde6d0364
lab314-13@labeng.local <lab314-13@labeng.local>
2020-01-03 15:22:56 +0700

    Number 2

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -2,11 +2,20 @@
 
 using namespace std;
 
-//Write function findDistance() here
-
+float findDistance(float u,float a,float t){
+  return u*t+(0.5*a)*t*t;
+}
 int main(){
-
-  //Calling findDistance() using test cases
+  float u,a,t;
+  cout<<"Enter u= ";
+  cin>>u;
+  cout<<"Enter a = ";
+  cin>>a;
+  cout<<"Enter t = ";
+  cin>>t;
+  cout<<"s = "<<findDistance(u,a,t);
+  
 
   return 0;
+  
 }
