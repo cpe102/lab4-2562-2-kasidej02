269f0035df5b6950d936e3db30e423bac98a1ffa
lab314-13@labeng.local <lab314-13@labeng.local>
2020-01-03 15:09:02 +0700

    Number 1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int x;
+    double y;
+    cout<<"Enter x = ";
+    cin>>x;
+    cout<<"Result y = "<<3*pow(x,3)+2*2.71828+pow(2,(2*x+1))+pow(pow(x,2)+1,0.5);
+    return 0;
+}
+
+
 
