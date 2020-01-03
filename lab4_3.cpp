02386ca40935ec07f359e56cebf5cd64ddf5394c
lab314-13@labeng.local <lab314-13@labeng.local>
2020-01-03 15:52:03 +0700

    Number 3

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
 
+int findDivisor(int x){
+    int i=2;
+    while(i>0){
+        if(x%i==0){
+            return i;
+        }else{
+            i++;
+        }
+    }
+}
+
+int main(){
+int x;
+cout<<"Enter input = ";
+cin>>x;
+cout<<"Answer is "<<findDivisor(x);
+return 0;
+}
